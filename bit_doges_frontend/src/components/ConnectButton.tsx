191663b60633f518523ede20c5686347f7370b57
meaj <meaj.mailbox@gmail.com>
2021-10-09 13:55:41 -0500

    Added metamask connection functionality

--- bit_doges_frontend/src/components/ConnectButton.tsx
+++ bit_doges_frontend/src/components/ConnectButton.tsx
@@ -5,14 +5,18 @@ import { useEthers, useEtherBalance } from "@usedapp/core";
 export default function ConnectButton() {
   const { activateBrowserWallet, account } = useEthers();
   const etherBalance = useEtherBalance(account);
-
+  
+  function handleConnectWallet() {
+    activateBrowserWallet();
+  }
+  
   return account ? (
-    <Box>
-      <Text color="whire" fontSize="md">
-        {etherBalance && etherBalance} ETH
+    <Box alignItems="center">
+      <Text color="white" fontSize="md">
+        Balance: {etherBalance && JSON.stringify(etherBalance)} ETH
       </Text>
     </Box>
   ) : (
-    <Button>Connect Wallet</Button>
+    <Button onClick={handleConnectWallet}>Connect Wallet</Button>
   );
 }
