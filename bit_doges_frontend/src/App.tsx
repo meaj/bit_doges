0ac7ec0afe155055b53bc0f067b42cbba7e23afc
meaj <meaj.mailbox@gmail.com>
2021-10-09 13:19:41 -0500

    Added hello world using chakra

--- bit_doges_frontend/src/App.tsx
+++ bit_doges_frontend/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.tsx
+import { ChakraProvider } from "@chakra-ui/react"
+import Layout from "./components/Layout";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChakraProvider>
+      <Layout>
+        <p style={{ color: "white" }}>What's good frens!</p>
+      </Layout>
+    </ChakraProvider>
+  )
 }
-
-export default App;
