cfd7d398fc20e60a0058cc092224b5b7786c1888
meaj <meaj.mailbox@gmail.com>
2021-10-10 12:20:14 -0500

    Added updated images and began layout tweaks

--- bit_doges_frontend/src/App.tsx
+++ bit_doges_frontend/src/App.tsx
@@ -1,12 +1,15 @@
 // App.tsx
 import ConnectButton from "./components/ConnectButton";
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, Heading } from "@chakra-ui/react"
 import Layout from "./components/Layout";
+import bitdogesGif from './static/BitDoges.gif'
 
 export default function App() {
   return (
     <ChakraProvider>
       <Layout>
+        <Heading size="xl" style={{ color: "gold" }}>BitDoges</Heading>
+        <img src={bitdogesGif} alt="Rotating BitDoges Logo"/>
         <p style={{ color: "white" }}>hemlo frens</p>
         <ConnectButton />
       </Layout>
