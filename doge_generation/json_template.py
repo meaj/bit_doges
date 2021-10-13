5ebe343c0dc9c722ef416a7a27cdd6d0636751c6
meaj <meaj.mailbox@gmail.com>
2021-10-12 20:33:50 -0500

    Removed dupes XD

--- doge_generation/json_template.py
+++
@@ -1,7 +0,0 @@
-doge_metadata_template = {
-    "name": "",
-    "description": "One of the many BitDoges roaming the Ethereum Network",
-    "external_url": "",
-    "image": "",
-    "attributes": []
-}
